eee8cfb72e58a9821a47f784d48d612ad9833241
Enmyr <natebarratt06@gmail.com>
2024-09-30 15:00:26 -0400

    Added Circle func. Needs to be tweaked still.

---
+++ Game.cs
@@ -0,0 +1,47 @@
+﻿// Include code libraries you need below (use the namespace).
+using System;
+using System.Numerics;
+
+// The namespace your code is in.
+namespace Game10003
+{
+    /// <summary>
+    ///     Your game code goes inside this class!
+    /// </summary>
+    public class Game
+    {
+        // Place your variables here:
+
+
+        /// <summary>
+        ///     Setup runs once before the game loop begins.
+        /// </summary>
+        public void Setup()
+        {
+            Window.SetTitle("Click the Dot");
+            Window.SetSize(400, 400);
+            Window.TargetFPS = 60;
+            //target = Color.White;
+        }
+
+        /// <summary>
+        ///     Update runs every frame.
+        /// </summary>
+        public void Update()
+        {
+            bool isClicking = Input.IsMouseButtonPressed(MouseInput.Left);
+            if (isClicking == true)
+            {
+                DrawTarget();
+            }
+        }
+        void DrawTarget()
+        {
+            Vector2 targetPos = new Vector2();
+            targetPos = Random.Vector2(360, 360);
+
+            Draw.FillColor = Color.OffWhite;
+            Draw.Circle(targetPos, 80);
+        }
+    }
+}
